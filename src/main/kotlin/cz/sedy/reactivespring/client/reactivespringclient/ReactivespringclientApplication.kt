36422304a7d4ea54c159f7e60b13aeeca672bbff
Jakub Šedý <sedyjaku@fit.cvut.cz>
2020-05-11 13:40:18 +0200

    Initial commit

---
+++ src/main/kotlin/cz/sedy/reactivespring/client/reactivespringclient/ReactivespringclientApplication.kt
@@ -0,0 +1,11 @@
+package cz.sedy.reactivespring.client.reactivespringclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactivespringclientApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactivespringclientApplication>(*args)
+}
---
+++ src/test/kotlin/cz/sedy/reactivespring/client/reactivespringclient/ReactivespringclientApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.sedy.reactivespring.client.reactivespringclient
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactivespringclientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
